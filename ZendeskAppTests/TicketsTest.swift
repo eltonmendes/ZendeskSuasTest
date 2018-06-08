8a347e23411f3c1adae5457afd29e9869ebb19ba
Elton Mendes <ejunior@cartrawler.com>
2018-06-08 16:20:29 +0100

    Adding tests

--- ZendeskAppTests/TicketsTest.swift
+++ ZendeskAppTests/TicketsTest.swift
@@ -9,7 +9,13 @@
 import XCTest
 import Suas
 
+struct TicketsTestConstraints {
+    static let viewID = "39551161"
+}
+
 class TicketsTest: XCTestCase {
+    
+    let zendeskAPI = ZendeskAPI()
     
     func testTicketsAvailableAsyncAction() {
         
@@ -32,8 +38,22 @@ class TicketsTest: XCTestCase {
         let action = TicketsFetchedAction(tickets: tickets)
         _ = reducer.reduce(state: state, action:action )
         XCTAssert(state.tickets.tickets.count == 0)
-
+        
     }
     
+    func testFetchTicketsAPIWithSuccess() {
+        
+        let expectation = XCTestExpectation(description: "Fetch Tickets from server")
+        
+        zendeskAPI.performTicketsSearch(viewID: TicketsTestConstraints.viewID) { (data, response, errorResponse) in
+            
+            XCTAssert(data != nil)
+            XCTAssert(errorResponse == nil)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10)
+    }
     
 }
+
